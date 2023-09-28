31e53df81a468507e687d50f6544e5703a8885b2
Sarah <Sarah@HP-SARAHI>
2023-09-27 18:03:39 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("ingrese un numero para el tamaño del arreglo.");
+int n = int.Parse(Console.ReadLine());
+//Declaramos el arreglo y a su vez le asignamos memoria
+int[] arreglo=new int[n];
+Console.WriteLine("Ingrese los valores del arreglo");
+//Pedimos el valor por cada elemento del arreglo
+for (int j=0; j < arreglo.Length; j++)
+{
+    Console.Write("Valor del sub-indice ["+j+"]:");
+    arreglo[j]=int.Parse(Console.ReadLine());
+}
+//Recorrer el arreglo para mostrar
+for (int j = 0; j < arreglo.Length; j++)
+{
+    Console.WriteLine(arreglo[j]);
+}
